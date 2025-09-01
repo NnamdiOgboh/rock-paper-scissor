9f5bf20cfa7babeb52f71dfad36d839f10486cd9
NnamdiOgboh <nnamdiogboh@gmail.com>
2025-09-01 14:02:50 +0100

    added human choice

--- test.js
+++ test.js
@@ -3,9 +3,24 @@ console.log('Hello World!')
 function getComputerChoice() {
   const randomNumber = Math.random();
 
-  let getComputerChoice = '';
+  let computerChoice = '';
 
   if (randomNumber >= 0 && randomNumber < 1/3) {
-    getComputerChoice = 'rock';
-  }else if
+    computerChoice = 'rock';
+  }else if (randomNumber >= 1/3 && randomNumber < 2/3) {
+    computerChoice = 'paper';
+  }else if (randomNumber >= 2/3 && randomNumber < 1) {
+    computerChoice = 'scissor';
+  }
+
+  return computerChoice;
 }
+
+console.log(getComputerChoice());
+
+function getHumanChoice() {
+  const pickChoice = prompt('Pick rock, paper or scissor')
+}
+
+console.log(getHumanChoice());
+
