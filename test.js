8c6a7bf6abc2938dad272455589c0c67b96d698e
NnamdiOgboh <nnamdiogboh@gmail.com>
2025-09-01 13:45:24 +0100

    first commit

---
+++ test.js
@@ -0,0 +1,11 @@
+console.log('Hello World!')
+
+function getComputerChoice() {
+  const randomNumber = Math.random();
+
+  let getComputerChoice = '';
+
+  if (randomNumber >= 0 && randomNumber < 1/3) {
+    getComputerChoice = 'rock';
+  }else if
+}
